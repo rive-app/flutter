005d6e23fed1bc499b5ceb272e6b7a93cec631bd
Luigi Rosso <luigi.rosso@gmail.com>
2019-12-20 14:34:02 -0800

    Cleanup

---
+++ server_library/test/change_test.dart
@@ -0,0 +1,10 @@
+import 'dart:typed_data';
+
+import 'package:coop_server_library/src/change.dart';
+import "package:test/test.dart";
+
+void main() {
+  test('serialize', () {
+    var change = Change();
+  });
+}
