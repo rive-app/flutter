4f6e65974f20045d87f056f0677d66554dcd8618
Luigi Rosso <luigi.rosso@gmail.com>
2020-01-13 10:25:49 -0800

    Cleanup

--- editor/lib/rive/stage/items/stage_artboard.dart
+++ editor/lib/rive/stage/items/stage_artboard.dart
@@ -3,7 +3,23 @@ import 'package:rive_core/artboard.dart';
 
 import '../stage_item.dart';
 
-class StageArtboard extends StageItem<Artboard> {
+class StageArtboard extends StageItem<Artboard> implements ArtboardDelegate {
+  AABB _aabb;
+
+  @override
+  bool initialize(Artboard object) {
+    if (!super.initialize(object)) {
+      return false;
+    }
+    object.delegate = this;
+    return true;
+  }
+
   @override
   AABB get aabb => null;
+
+  @override
+  void markBoundsDirty() {
+      // TODO: figure out how to debounce this for next stage render.
+  }
 }
--- rive_core/lib/artboard.dart
+++ rive_core/lib/artboard.dart
@@ -1,4 +1,17 @@
 import 'src/generated/artboard_base.dart';
 export 'src/generated/artboard_base.dart';
 
-class Artboard extends ArtboardBase {}
+abstract class ArtboardDelegate {
+  void markBoundsDirty();
+}
+
+class Artboard extends ArtboardBase {
+  ArtboardDelegate delegate;
+
+  @override
+  void widthChanged(double from, double to) {
+    super.widthChanged(from, to);
+    delegate?.markBoundsDirty();
+
+  }
+}
