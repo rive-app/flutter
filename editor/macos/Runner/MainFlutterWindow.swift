3fdc61ac47d63a35aedf0cf2921dde4906cb3f36
Rody Davis <rody@rive.app>
2020-01-22 09:17:23 -0800

    updating macos project

--- editor/macos/Runner/MainFlutterWindow.swift
+++ editor/macos/Runner/MainFlutterWindow.swift
@@ -1,15 +1,15 @@
-import Cocoa
-import FlutterMacOS
-
-class MainFlutterWindow: NSWindow {
-  override func awakeFromNib() {
-    let flutterViewController = FlutterViewController.init()
-    let windowFrame = self.frame
-    self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
-
-    RegisterGeneratedPlugins(registry: flutterViewController)
-
-    super.awakeFromNib()
-  }
-}
+//import Cocoa
+//import FlutterMacOS
+//
+//class MainFlutterWindow: NSWindow {
+//  override func awakeFromNib() {
+//    let flutterViewController = FlutterViewController.init()
+//    let windowFrame = self.frame
+//    self.contentViewController = flutterViewController
+//    self.setFrame(windowFrame, display: true)
+//
+//    RegisterGeneratedPlugins(registry: flutterViewController)
+//
+//    super.awakeFromNib()
+//  }
+//}
