b89f1afe425d54e1ba503dc38b7744f414e174d8
Matt Sullivan <matt@rive.app>
2020-01-23 15:30:44 -0800

    Added unit tests

--- test/tree_widget_test.dart
+++ test/tree_widget_test.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/gestures/drag_details.dart';
 import 'package:flutter/src/gestures/events.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tree_widget/flat_tree_item.dart';
 import 'package:tree_widget/tree_controller.dart';
+import 'package:tree_widget/tree_widget.dart';
 
 class TreeItem {
   final String name;
   final List<TreeItem> children;
 
-  TreeItem(this.name, {this.children});
+  const TreeItem(this.name, {this.children});
 }
 
 class PropertyTreeItem extends TreeItem {
-  PropertyTreeItem(String name, {List<TreeItem> children})
+  const PropertyTreeItem(String name, {List<TreeItem> children})
       : super(name, children: children);
 }
 
 class MyTreeController extends TreeController<TreeItem> {
   MyTreeController(List<TreeItem> data) : super(data);
-
 
   @override
   List<TreeItem> childrenOf(TreeItem treeItem) {
@@ -62,35 +63,132 @@ class MyTreeController extends TreeController<TreeItem> {
 }
 
 void main() {
-  test('test tree flattening', () {
-    var data = [
-      TreeItem(
-        "Artboard",
-        children: [
-          TreeItem("Group"),
-          TreeItem("body"),
-          TreeItem("neck"),
-          TreeItem("leg_right"),
-          TreeItem("head"),
-          TreeItem("leg_left"),
-          TreeItem("root", children: [
-            TreeItem("ik_head", children: [
-              PropertyTreeItem("Translation Constraint"),
-              TreeItem("neck"),
-            ]),
-            TreeItem("ctrl_foot_left"),
-            TreeItem("ctrl_foot_right"),
-          ]),
-          TreeItem("leg_left"),
-          TreeItem("leg_right"),
-        ],
-      )
-    ];
+  TreeItem treeRoot;
 
-    var controller = MyTreeController(data);
-    controller.flatten();
-    controller.expand(data[0]);
-    controller.expand(data[0].children[6]);
-    expect(controller.flat.length, 13);
+  setUp(() {
+    treeRoot = const TreeItem(
+      'Artboard',
+      children: [
+        TreeItem('Group'),
+        TreeItem('body'),
+        TreeItem('neck'),
+        TreeItem('arm_right'),
+        TreeItem('head'),
+        TreeItem('arm_left'),
+        TreeItem('root', children: [
+          TreeItem('ik_head', children: [
+            PropertyTreeItem('Translation Constraint'),
+            TreeItem('neck'),
+          ]),
+          TreeItem('ctrl_foot_left'),
+          TreeItem('ctrl_foot_right'),
+        ]),
+        TreeItem('leg_left'),
+        TreeItem('leg_right'),
+      ],
+    );
+  });
+
+  group('Tree Tests', () {
+    test('test tree flattening', () {
+      final data = [treeRoot];
+      final controller = MyTreeController(data);
+
+      controller.flatten();
+      controller.expand(data[0]);
+      controller.expand(data[0].children[6]);
+      expect(controller.flat.length, 13);
+    });
+  });
+
+  group('Widget Tests', () {
+    testWidgets('Instantiate a TreeView widget', (tester) async {
+      final data = [treeRoot];
+      final controller = MyTreeController(data);
+
+      // Tree should start with only one item viewable
+      controller.flatten();
+      expect(controller.flat.length, 1);
+
+      final treeWidget = TreeView<TreeItem>(
+        controller: controller,
+        expanderBuilder: (context, item) => Container(),
+        itemBuilder: (context, item) => Text(item.data.name),
+        iconBuilder: (context, item) => Container(),
+      );
+
+      final wrappedWidget = MediaQuery(
+        data: const MediaQueryData(),
+        child: Directionality(
+          textDirection: TextDirection.ltr,
+          child: treeWidget,
+        ),
+      );
+
+      await tester.pumpWidget(wrappedWidget);
+      expect(find.text('Artboard'), findsOneWidget);
+    });
+
+    testWidgets('TreeView widget displays an item\'s icon', (tester) async {
+      final data = [treeRoot];
+      final controller = MyTreeController(data)..flatten();
+
+      final iconWidget = Icon(Icons.block);
+
+      final treeWidget = TreeView<TreeItem>(
+        controller: controller,
+        expanderBuilder: (context, item) => Container(),
+        itemBuilder: (context, item) => Text(item.data.name),
+        iconBuilder: (context, item) => iconWidget,
+      );
+
+      final wrappedWidget = MediaQuery(
+        data: const MediaQueryData(),
+        child: Directionality(
+          textDirection: TextDirection.ltr,
+          child: treeWidget,
+        ),
+      );
+
+      await tester.pumpWidget(wrappedWidget);
+      expect(find.byWidget(iconWidget), findsOneWidget);
+    });
+
+    testWidgets('TreeView widget can be expanded', (tester) async {
+      final data = [treeRoot];
+      final controller = MyTreeController(data);
+
+      // Tree should start with only one item viewable
+      controller.flatten();
+      expect(controller.flat.length, 1);
+
+      // Expand the root tree
+      controller.expand(treeRoot);
+      // Ten items should now be visible
+      expect(controller.flat.length, 10);
+      controller.flatten();
+
+      final treeWidget = TreeView<TreeItem>(
+        controller: controller,
+        expanderBuilder: (context, item) => Container(),
+        itemBuilder: (context, item) => Text(item.data.name),
+        iconBuilder: (context, item) => Container(),
+      );
+
+      final wrappedWidget = MediaQuery(
+        data: const MediaQueryData(),
+        child: Directionality(
+          textDirection: TextDirection.ltr,
+          child: treeWidget,
+        ),
+      );
+
+      await tester.pumpWidget(wrappedWidget);
+      expect(find.text('head'), findsOneWidget);
+      expect(find.text('leg_left'), findsOneWidget);
+      expect(find.text('leg_right'), findsOneWidget);
+      expect(find.text('arm_left'), findsOneWidget);
+      expect(find.text('arm_right'), findsOneWidget);
+    });
   });
 }
