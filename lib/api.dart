4be2e5cb0dcb4b8277f32a1971c093c1b74a229f
Luigi Rosso <luigi.rosso@gmail.com>
2020-01-24 19:52:55 -0800

    Use arkham.rive.app.

--- lib/api.dart
+++ lib/api.dart
@@ -2,7 +2,7 @@ import 'src/web_service_client.dart';
 export 'src/http_exception.dart';
 
 class RiveApi extends WebServiceClient {
-  final String host = 'http://localhost:3000';
-  // final String host = 'https://rive.app';
+  // final String host = 'http://localhost:3000';
+  final String host = 'https://arkham.rive.app';
   RiveApi() : super('rive-api');
 }
