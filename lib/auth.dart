3a80ed874d04a9906eb3052b63222ea7c88eae4c
Luigi Rosso <luigi.rosso@gmail.com>
2020-01-26 14:04:50 -0800

    Adding OAuth for Google.

--- lib/auth.dart
+++ lib/auth.dart
@@ -28,26 +28,19 @@ class RiveAuth {
     return false;
   }
 
-  /// Initiate an OAuth login session with Twitter.
-  Future<bool> loginTwitter() async {
-    assert(!kIsWeb, "Shouldn't be authenticating from Flutter Web.");
-    String spectre = await WindowUtils.openWebView(
-        'auth', api.host + '/desktop/signin/twitter',
-        size: const Size(500, 600), jsMessage: 'jsHandler');
-    if (spectre != null) {
-      api.setCookie('spectre', spectre);
-      await WindowUtils.closeWebView('auth');
-      await api.persist();
-    }
-
-    return true;
-  }
-
   /// Initiate an OAuth login session with Facebook.
-  Future<bool> loginFacebook() async {
-    assert(!kIsWeb, "Shouldn't be authenticating from Flutter Web.");
+  Future<bool> loginFacebook() => _loginOAuth('facebook');
+
+  /// Initiate an OAuth login session with Twitter.
+  Future<bool> loginTwitter() => _loginOAuth('twitter');
+
+  /// Initiate an OAuth login session with Google.
+  Future<bool> loginGoogle() => _loginOAuth('google');
+
+  Future<bool> _loginOAuth(String provider) async {
+    assert(!kIsWeb, 'Shouldn\'t be authenticating from Flutter Web.');
     String spectre = await WindowUtils.openWebView(
-        'auth', api.host + '/desktop/signin/facebook',
+        'auth', api.host + '/desktop/signin/$provider',
         size: const Size(500, 600), jsMessage: 'jsHandler');
     if (spectre != null) {
       api.setCookie('spectre', spectre);
