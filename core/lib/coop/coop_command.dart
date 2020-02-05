01a85b38b7bcc5e12c5a06a4fdfec4c1c453e570
Luigi Rosso <luigi.rosso@gmail.com>
2020-02-04 20:25:48 -0800

    Fixing numbers.

--- core/lib/coop/coop_command.dart
+++ core/lib/coop/coop_command.dart
@@ -3,10 +3,10 @@ class CoopCommand {
   static const int ready = 1;
   static const int goodbye = 2;
   static const int cursor = 3;
-  static const int synchronize = 3;
-  static const int wipe = 4;
-  static const int accept = 5;
-  static const int reject = 6;
-  static const int changeId = 7;
-  static const int minChangeId = 8;
+  static const int synchronize = 4;
+  static const int wipe = 5;
+  static const int accept = 6;
+  static const int reject = 7;
+  static const int changeId = 8;
+  static const int minChangeId = 9;
 }
