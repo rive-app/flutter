2ad12fe4eadc7850ede5e50a6036d435a0c91194
Matt Sullivan <matt@rive.app>
2020-02-07 15:35:34 -0800

    Fixed broken unit tests in Rive Core

--- rive_core/test/server_connect_test.dart
+++ rive_core/test/server_connect_test.dart
@@ -7,49 +7,53 @@ import 'package:core/coop/coop_isolate.dart';
 import 'src/test_server.dart';
 
 void main() {
-  test('connecting to server', () async {
-    TestWidgetsFlutterBinding.ensureInitialized();
+  test(
+    'connecting to server',
+    () async {
+      TestWidgetsFlutterBinding.ensureInitialized();
 
-    final server = TestCoopServer();
-    expect(await server.listen(port: 8124), true);
-    // Create fake server file.
-    final file = RiveFile("fake",
-        overridePreferences: <String, dynamic>{
-          "session": 22,
-          "token": "fake",
-          // "lastServerChangeId": 0
-        },
-        useSharedPreferences: false);
-    expect(await file.connect('ws://localhost:8124/1203/3203'),
-        ConnectResult.connected);
+      final server = TestCoopServer();
+      expect(await server.listen(port: 8124), true);
 
-    // make sure server has one connected client and file.
-    expect(server.editingFileCount, 1,
-        reason: "Server should have one editing file.");
-    expect(server.clientCount, 1,
-        reason: "Server should have one client connected.");
+      // Create fake server file.
+      final file = RiveFile('fake', 'fake',
+          overridePreferences: <String, dynamic>{
+            "session": 22,
+            "token": "fake",
+            // "lastServerChangeId": 0
+          },
+          useSharedPreferences: false);
 
-    print('connected');
-    var node = file.add(Node()..name = 'test');
-    node.name = 'name change';
-    file.captureJournalEntry();
-    node.name = 'name change2';
-    file.captureJournalEntry();
-    // Give opportunity to save...
-    await Future.delayed(const Duration(milliseconds: 1000), () {});
+      expect(await file.connect('ws://localhost:8124', '1203/3203/fake'),
+          ConnectResult.connected);
 
-    print("disconnect");
-    expect(await file.disconnect(), true);
-    print("Waiting for isolate shutdown...");
-    // give server opportunity to catch up...
-    await Future.delayed(CoopIsolate.killTimeout, () {});
-    await Future.delayed(const Duration(milliseconds: 100), () {});
+      // make sure server has one connected client and file.
+      expect(server.editingFileCount, 1,
+          reason: "Server should have one editing file.");
+      expect(server.clientCount, 1,
+          reason: "Server should have one client connected.");
 
-    expect(server.editingFileCount, 0,
-        reason: "Editing file count should be 0");
-    expect(server.clientCount, 0);
-    expect(await server.close(), true);
+      var node = file.add(Node()..name = 'test');
+      node.name = 'name change';
+      file.captureJournalEntry();
+      node.name = 'name change2';
+      file.captureJournalEntry();
+      // Give opportunity to save...
+      await Future.delayed(const Duration(milliseconds: 1000), () {});
 
-    print('done');
-  });
+      expect(await file.disconnect(), true);
+
+      // give server opportunity to catch up...
+      await Future.delayed(CoopIsolate.killTimeout, () {});
+      await Future.delayed(const Duration(milliseconds: 100), () {});
+
+      expect(server.editingFileCount, 0,
+          reason: "Editing file count should be 0");
+      expect(server.clientCount, 0);
+      expect(await server.close(), true);
+    },
+    timeout: const Timeout(
+      Duration(seconds: 15),
+    ),
+  );
 }
--- rive_core/test/src/test_server.dart
+++ rive_core/test/src/test_server.dart
@@ -6,7 +6,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:core/coop/change.dart';
 import 'package:core/coop/coop_server_client.dart';
 import 'package:core/coop/coop_server.dart';
-import 'package:core/coop/coop_user.dart';
 import 'package:rive_core/data_directory.dart';
 import 'package:core/coop/coop_isolate.dart';
 
@@ -18,20 +17,16 @@ class _TestCoopIsolate extends CoopIsolateProcess {
   @override
   Future<bool> initialize(int ownerId, int fileId,
       [Map<String, String> options]) async {
-    this.ownerId = ownerId;
-    this.fileId = fileId;
-    _dataDir = await dataDirectory("test_coop_server");
-    return _dataDir != null;
+    //this.ownerId = ownerId;
+    //this.fileId = fileId;
+    return true;
   }
 
-  @override
   Future<Uint8List> loadData(String key) async {
-    print("WRITING TO KEY $key ${_dataDir.path + key}");
     var file = File(_dataDir.path + key);
     return file.readAsBytes();
   }
 
-  @override
   Future<bool> saveData(String key, Uint8List data) async {
     var file = File(_dataDir.path + key);
     await file.writeAsBytes(data, flush: true);
@@ -40,14 +35,7 @@ class _TestCoopIsolate extends CoopIsolateProcess {
 
   @override
   Future<bool> shutdown() async {
-    await _dataDir.delete(recursive: true);
     return true;
-  }
-
-  @override
-  Future<CoopUser> login(String token) async {
-    // In test mode we validate any user...
-    return CoopUser(1);
   }
 
   @override
@@ -62,9 +50,15 @@ class _TestCoopIsolate extends CoopIsolateProcess {
   }
 
   @override
-  ChangeSet initialChanges() {
+  ChangeSet buildFileChangeSet() {
     // TODO: implement initialChanges
     return null;
+  }
+
+  @override
+  Future<void> persist() {
+    // TODO: implement persist
+    return Future<void>.value();
   }
 }
 
@@ -79,7 +73,9 @@ class TestCoopServer extends CoopServer {
   }
 
   @override
-  Future<bool> validate(HttpRequest request, int ownerId, int fileId) async {
-    return true;
+  Future<int> validate(
+      HttpRequest request, int ownerId, int fileId, String token) {
+    // TODO: implement validate
+    return Future.value(1);
   }
 }
