ddb8b1dcc0ac5c6b1df653bfb1468667491e70a8
Rody Davis <rody@rive.app>
2020-02-20 11:44:52 -0800

    adding animation controller

--- editor/lib/widgets/toolbar/design_animate_toggle.dart
+++ editor/lib/widgets/toolbar/design_animate_toggle.dart
@@ -7,60 +7,82 @@ class DesignAnimateToggle extends StatefulWidget {
   _DesignAnimateToggleState createState() => _DesignAnimateToggleState();
 }
 
-class _DesignAnimateToggleState extends State<DesignAnimateToggle> {
+class _DesignAnimateToggleState extends State<DesignAnimateToggle>
+    with SingleTickerProviderStateMixin {
   bool isAnimate = false;
+  AnimationController controller;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    const _duration = Duration(milliseconds: 100);
+    controller = AnimationController(
+      vsync: this,
+      duration: _duration,
+      reverseDuration: _duration,
+    );
+    final curve = CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeInOutCubic,
+    );
+    animation = Tween<double>(
+      begin: 0,
+      end: kAnimateToggleWidth / 2,
+    ).animate(curve)
+      ..addListener(() => setState(() {}));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: kAnimateToggleWidth,
-      child: Stack(
-        children: <Widget>[
-          Positioned.fill(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: const Color.fromRGBO(47, 47, 47, 1.0),
+      child: LayoutBuilder(
+        builder: (_, dimens) => Stack(
+          children: <Widget>[
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: const Color.fromRGBO(47, 47, 47, 1.0),
+                ),
               ),
             ),
-          ),
-          AnimatedPositioned(
-            duration: const Duration(milliseconds: 400),
-            top: 0,
-            bottom: 0,
-            curve: Curves.easeIn,
-            left: isAnimate ? null : 0,
-            right: !isAnimate ? null : 0,
-            width: kAnimateToggleWidth / 2,
-            child: Container(
+            Positioned(
+              top: 0,
+              bottom: 0,
+              left: animation.value,
               width: kAnimateToggleWidth / 2,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: const Color.fromRGBO(68, 68, 68, 1.0),
+              child: Container(
+                margin: const EdgeInsets.all(1.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: const Color.fromRGBO(68, 68, 68, 1.0),
+                ),
               ),
             ),
-          ),
-          Positioned(
-            top: 0,
-            bottom: 0,
-            left: 0,
-            width: kAnimateToggleWidth / 2,
-            child: GestureDetector(
-              onTap: () => _setAnimate(false),
-              child: _buildText('Design', !isAnimate),
+            Positioned(
+              left: 0,
+              width: kAnimateToggleWidth / 2,
+              height: dimens.maxHeight,
+              child: GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onTap: () => _setAnimate(false),
+                child: _buildText('Design', !isAnimate),
+              ),
             ),
-          ),
-          Positioned(
-            top: 0,
-            bottom: 0,
-            right: 0,
-            width: kAnimateToggleWidth / 2,
-            child: GestureDetector(
-              onTap: () => _setAnimate(true),
-              child: _buildText('Animate', isAnimate),
+            Positioned(
+              right: 0,
+              width: kAnimateToggleWidth / 2,
+              height: dimens.maxHeight,
+              child: GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onTap: () => _setAnimate(true),
+                child: _buildText('Animate', isAnimate),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -75,6 +97,12 @@ class _DesignAnimateToggleState extends State<DesignAnimateToggle> {
   }
 
   void _setAnimate(bool value) {
-    if (mounted) setState(() => isAnimate = value);
+    isAnimate = value;
+    // if (mounted) setState(() => isAnimate = value);
+    if (isAnimate) {
+      controller.forward();
+    } else {
+      controller.reverse();
+    }
   }
 }
