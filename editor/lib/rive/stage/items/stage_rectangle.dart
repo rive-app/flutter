089735c6567c998707419290a7b97146236e3389
Rody Davis <rody@rive.app>
2020-02-20 15:37:21 -0800

    updating rectangle

--- editor/lib/rive/stage/items/stage_rectangle.dart
+++ editor/lib/rive/stage/items/stage_rectangle.dart
@@ -1,6 +1,9 @@
 import 'package:rive_core/bounds_delegate.dart';
 import 'package:rive_core/math/aabb.dart';
+import 'package:rive_core/node.dart';
+import 'package:rive_core/shapes/parametric_path.dart';
 import 'package:rive_core/shapes/rectangle.dart';
+import 'package:rive_editor/rive/inspectable.dart';
 
 import '../stage_item.dart';
 
@@ -13,4 +16,16 @@ class StageRectangle extends StageItem<Rectangle> with BoundsDelegate {
     assert(stage != null);
     stage.updateBounds(this);
   }
+
+  @override
+  Set<InspectorBase> get inspectorItems => {
+        InspectorItem(name: 'Pos', propertyKeys: [
+          NodeBase.xPropertyKey,
+          NodeBase.yPropertyKey,
+        ]),
+        InspectorItem(name: 'Size', propertyKeys: [
+          ParametricPathBase.widthPropertyKey,
+          ParametricPathBase.heightPropertyKey,
+        ])
+      };
 }
