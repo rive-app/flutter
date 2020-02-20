0328e7db38b8d1326d4a73fbb30262b77be80a5f
Rody Davis <rody@rive.app>
2020-02-19 16:13:48 -0800

    adding RectangleTool test

--- editor/test/tools_test.dart
+++ editor/test/tools_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rive_editor/rive/stage/tools/ellipse_tool.dart';
+import 'package:rive_editor/rive/stage/tools/rectangle_tool.dart';
 
 void main() {
   group("EllipseTool", () {
@@ -15,6 +16,22 @@ void main() {
 
     test('Check for icon name', () {
       expect(instance.icon, 'tool-ellipse');
+    });
+  });
+
+  group("RectangleTool", () {
+    RectangleTool instance;
+
+    setUp(() {
+      instance = RectangleTool.instance;
+    });
+
+    test('Check instance', () {
+      expect(instance != null, true);
+    });
+
+    test('Check for icon name', () {
+      expect(instance.icon, 'tool-rectangle');
     });
   });
 }
