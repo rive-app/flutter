4bcbb2ca35d8f5cd584a045debba9759cfe42791
Rody Davis <rody@rive.app>
2020-02-19 17:26:09 -0800

    updating theme and adding empty property inspector

--- editor/lib/main.dart
+++ editor/lib/main.dart
@@ -33,6 +33,7 @@ import 'widgets/popup/popup.dart';
 import 'widgets/resize_panel.dart';
 import 'widgets/stage_view.dart';
 import 'widgets/tab_bar/rive_tab_bar.dart';
+import 'widgets/theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -360,25 +361,50 @@ class Editor extends StatelessWidget {
                           .whereType<StageArtboard>()
                           .map((stageItem) => stageItem.component)
                           .toList(growable: false);
-
+                      if (artboards.isEmpty) {
+                        return Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                  "No Selection",
+                                  style: TextStyle(
+                                    color: ThemeUtils.textWhite,
+                                    fontSize: 13,
+                                  ),
+                                ),
+                              ),
+                              Container(height: 10.0),
+                              Container(
+                                child: Text(
+                                  "Select something to view its properties and options.",
+                                  style: TextStyle(
+                                    color: ThemeUtils.textGreyLight,
+                                    fontSize: 13,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
                       return Column(
                         children: [
-                          artboards.isEmpty
-                              ? Container()
-                              : PropertyDual(
-                                  name: 'Pos',
-                                  objects: artboards,
-                                  propertyKeyA: ArtboardBase.xPropertyKey,
-                                  propertyKeyB: ArtboardBase.yPropertyKey,
-                                ),
-                          artboards.isEmpty
-                              ? Container()
-                              : PropertyDual(
-                                  name: 'Size',
-                                  objects: artboards,
-                                  propertyKeyA: ArtboardBase.widthPropertyKey,
-                                  propertyKeyB: ArtboardBase.heightPropertyKey,
-                                ),
+                          PropertyDual(
+                            name: 'Pos',
+                            objects: artboards,
+                            propertyKeyA: ArtboardBase.xPropertyKey,
+                            propertyKeyB: ArtboardBase.yPropertyKey,
+                          ),
+                          PropertyDual(
+                            name: 'Size',
+                            objects: artboards,
+                            propertyKeyA: ArtboardBase.widthPropertyKey,
+                            propertyKeyB: ArtboardBase.heightPropertyKey,
+                          ),
                           // selection. PropertyDual()
                         ],
                       );
