d8027e29743a8ffd2c46e3bbae94ec6947eb825d
Rody Davis <rody@rive.app>
2020-02-20 17:32:36 -0800

    color picker

--- editor/lib/widgets/inspector/inspector_panel.dart
+++ editor/lib/widgets/inspector/inspector_panel.dart
@@ -4,6 +4,7 @@ import 'package:rive_core/selectable_item.dart';
 import 'package:rive_editor/rive/inspectable.dart';
 import 'package:rive_editor/rive/selection_context.dart';
 import 'package:rive_editor/rive/stage/items/stage_artboard.dart';
+import 'package:rive_editor/widgets/common/color_picker.dart';
 import 'package:rive_editor/widgets/common/custom_expansion_tile.dart';
 import 'package:rive_editor/widgets/inherited_widgets.dart';
 import 'package:rive_editor/widgets/listenable_builder.dart';
@@ -61,29 +62,51 @@ class InspectorPanel extends StatelessWidget {
               .toList(growable: false);
           return ValueListenableBuilder<Set<InspectorBase>>(
             valueListenable: rive.inspectorController.itemsListenable,
-            builder: (_, items, __) => ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                final item = items.toList()[index];
-                if (item is InspectorGroup) {
-                  return CustomExpansionTile(
-                    title: Text(item.name),
-                    initiallyExpanded: item.isExpanded.value,
-                    expanded: item.isExpanded,
-                    children: <Widget>[
-                      for (var child in item.children) ...[
-                        if (child is InspectorItem) ...[
-                          buildItem(child, artboards),
-                        ],
-                      ]
-                    ],
-                  );
-                }
-                if (item is InspectorItem) {
-                  return buildItem(item, artboards);
-                }
-                return Container();
-              },
+            builder: (_, items, __) => Column(
+              children: <Widget>[
+                RaisedButton(
+                  child: const Text('Color Picker'),
+                  onPressed: () {
+                    showDialog<Color>(
+                      context: context,
+                      builder: (context) {
+                        return Center(
+                          child: Container(
+                            width: 210.0,
+                            child: RiveColorPicker(),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      final item = items.toList()[index];
+                      if (item is InspectorGroup) {
+                        return CustomExpansionTile(
+                          title: Text(item.name),
+                          initiallyExpanded: item.isExpanded.value,
+                          expanded: item.isExpanded,
+                          children: <Widget>[
+                            for (var child in item.children) ...[
+                              if (child is InspectorItem) ...[
+                                buildItem(child, artboards),
+                              ],
+                            ]
+                          ],
+                        );
+                      }
+                      if (item is InspectorItem) {
+                        return buildItem(item, artboards);
+                      }
+                      return Container();
+                    },
+                  ),
+                ),
+              ],
             ),
           );
         },
