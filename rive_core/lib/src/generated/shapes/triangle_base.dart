ff5983f40fe50fcbbd89e0df63961628164c9281
Rody Davis <rody@rive.app>
2020-02-21 16:34:12 -0800

    Squashed commit of the following:
    commit 823c846d9d53e262cc3d281eb48a836cb4ff89a9
    Author: Rody Davis <rody@rive.app>
    Date:   Fri Feb 21 11:16:01 2020 -0800
    
        updating macos name
    
    commit f7be524309d8afec1ad539da44a6ea6b5fcaed15
    Merge: 5756a78 879f5f7
    Author: Matt Sullivan <matt@rive.app>
    Date:   Fri Feb 21 09:24:27 2020 -0800
    
        Merge branch 'master' of github.com:rive-app/rive-editor-flutter
    
    commit 5756a787d671252a5f4d7c267f4b3de49f077595
    Author: Matt Sullivan <matt@rive.app>
    Date:   Fri Feb 21 09:24:03 2020 -0800
    
        Updating dependencies
    
    commit 879f5f75f66abed737111b0ad2ece4956acfc4ee
    Merge: 96f4291 9f7e275
    Author: Maxwell Talbot <talbot.maxwell@gmail.com>
    Date:   Fri Feb 21 09:07:15 2020 -0800
    
        Merge pull request #26 from rive-app/new-icon
    
        New Rive Icon
    
    commit 9f7e27531ce294706a5fa703d4b2e6d3f95dff29
    Author: Rody Davis <rody@rive.app>
    Date:   Thu Feb 20 19:27:32 2020 -0800
    
        updating circle icon
    
    commit 96f429165736a8dbea76799537b1617a7dad10c3
    Merge: 0c55af3 87d8c03
    Author: Matt Sullivan <matt.j.sullivan@gmail.com>
    Date:   Thu Feb 20 19:20:05 2020 -0800
    
        Merge pull request #25 from rive-app/R20-165+1
    
        fixed fuzzy separator
    
    commit d3bc3ae84904728f94009aae057c5e2100c8bc6d
    Author: Rody Davis <rody@rive.app>
    Date:   Thu Feb 20 19:19:39 2020 -0800
    
        updating icon
    
    commit 87d8c038c399a72d886c24b6b31aa9716408f00d
    Author: Maxwell Talbot <talbot.maxwell@gmail.com>
    Date:   Thu Feb 20 19:18:00 2020 -0800
    
        updated dependencies to point to their respective master branches
    
    commit f80d90bb3a550cf722130a167b3d65e7496e3bd8
    Author: Rody Davis <rody@rive.app>
    Date:   Thu Feb 20 19:17:21 2020 -0800
    
        updating icon
    
    commit 9e0876a81a9a12dba53acc4231a7c33719e99ba0
    Author: Maxwell Talbot <talbot.maxwell@gmail.com>
    Date:   Thu Feb 20 19:15:19 2020 -0800
    
        fixed fuzzy separator
    
    commit 0c55af3d3d8f39512d3a2cff905d85eb98d0bef5
    Merge: b5984db 558464e
    Author: Maxwell Talbot <talbot.maxwell@gmail.com>
    Date:   Thu Feb 20 18:40:27 2020 -0800
    
        Merge pull request #24 from rive-app/R20-177
    
        R20 177 (R20 204)
    
    commit 558464e884c23ba994fa14062a04d3cf2637a1d2
    Author: Maxwell Talbot <talbot.maxwell@gmail.com>
    Date:   Thu Feb 20 16:57:30 2020 -0800
    
        updated dependencies to point to master
    
    commit 9a3c4299a9b389b6a48fe4145ef14d85d0691827
    Author: Maxwell Talbot <talbot.maxwell@gmail.com>
    Date:   Thu Feb 20 16:48:34 2020 -0800
    
        dry'ed up the shape-based tools
    
    commit 748ab823f14b57657cc8d52bda60d79a2e2a039c
    Author: Maxwell Talbot <talbot.maxwell@gmail.com>
    Date:   Thu Feb 20 15:31:04 2020 -0800
    
        Added bootstrap and pre-commit hook to run tests on commit
    
    commit 15940cc023e93ef96ef2d3868135df799765713f
    Author: Maxwell Talbot <talbot.maxwell@gmail.com>
    Date:   Thu Feb 20 15:27:04 2020 -0800
    
        tweaked tests
    
    commit 7eb7b1955c1a1b44eece0199afb0ff8c0f247c23
    Author: Maxwell Talbot <talbot.maxwell@gmail.com>
    Date:   Thu Feb 20 15:12:32 2020 -0800
    
        Added tests for triangle and rectangle shapes
    
    commit 6be779c883fb7966d2f9e10af26340eb2a8d2862
    Author: Maxwell Talbot <talbot.maxwell@gmail.com>
    Date:   Thu Feb 20 14:52:48 2020 -0800
    
        Added Triangle tool support to rive editor
    
    commit 48734a8166fc28ddd478f08dcddf4f4bc321bdd8
    Author: Maxwell Talbot <talbot.maxwell@gmail.com>
    Date:   Thu Feb 20 13:44:12 2020 -0800
    
        added basic triangle shape support to rive core

---
+++ rive_core/lib/src/generated/shapes/triangle_base.dart
@@ -0,0 +1,10 @@
+/// Core automatically generated lib/src/generated/shapes/triangle_base.dart.
+/// Do not modify manually.
+
+import '../../../shapes/parametric_path.dart';
+
+abstract class TriangleBase extends ParametricPath {
+  static const int typeKey = 8;
+  @override
+  int get coreType => TriangleBase.typeKey;
+}
