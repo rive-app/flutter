04a535ddf0fbf5cbc090473c55cf0c81042c3ed3
Maxwell Talbot <talbot.maxwell@gmail.com>
2020-02-24 13:52:41 -0800

    added basic popup context for the hamburger menu

--- editor/lib/widgets/toolbar/hamburger_popup_button.dart
+++ editor/lib/widgets/toolbar/hamburger_popup_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:rive_editor/widgets/popup/context_popup.dart';
+import 'package:rive_editor/widgets/popup/modal_popup.dart';
 import 'package:rive_editor/widgets/rive_popup_button.dart';
 import 'package:rive_editor/widgets/tinted_icon.dart';
 
@@ -12,6 +14,53 @@ class HamburgerPopupButton extends StatelessWidget {
             isHovered ? Colors.white : const Color.fromRGBO(140, 140, 140, 1),
         icon: 'tool-menu',
       ),
+      width: 267,
+      contextItems: [
+        PopupContextItem(
+          "File Name",
+        ),
+        PopupContextItem(
+          "Team Permissions",
+          select: () => _showModal(context, (_) => Container()),
+        ),
+        PopupContextItem(
+          "Revision History",
+          select: () => _showModal(context, (_) => Container()),
+        ),
+        PopupContextItem.separator(),
+        PopupContextItem(
+          "New File",
+          icon: 'add',
+        ),
+        PopupContextItem(
+          "New Folder",
+          icon: 'popup-folder',
+        ),
+        PopupContextItem.separator(),
+        PopupContextItem(
+          "Manual",
+        ),
+        PopupContextItem(
+          "Shortcuts",
+        ),
+        PopupContextItem(
+          "Report an Issue",
+        ),
+        PopupContextItem(
+          "Request a Feature",
+        ),
+        PopupContextItem(
+          "Work with Us!",
+        ),
+      ],
     );
+  }
+
+  void _showModal(BuildContext context, WidgetBuilder builder) {
+    ModalPopup(
+      builder: builder,
+      size: const Size(750, 629),
+      elevation: 20,
+    ).show(context);
   }
 }
