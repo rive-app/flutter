da505d1a7f66e0623df234f52b1ec9aad72303fd
Maxwell Talbot <talbot.maxwell@gmail.com>
2020-02-26 11:57:06 -0800

    made dismissOnSelect default to true

--- editor/lib/widgets/popup/list_popup.dart
+++ editor/lib/widgets/popup/list_popup.dart
@@ -17,8 +17,8 @@ abstract class PopupListItem {
   /// Height for the item in the popup list.
   double get height;
 
-  /// Wether selection of the item will result in dismissing the .
-  bool get dismissOnSelect;
+  /// Whether selection of the item will result in dismissing the .
+  bool get dismissOnSelect => true;
 
   /// Child popup displayed when this list item is hovered over.
   List<PopupListItem> get popup;
