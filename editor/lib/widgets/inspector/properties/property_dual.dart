ca586b094476d7d444ac4cf03ce61aa44e07070d
Maxwell Talbot <talbot.maxwell@gmail.com>
2020-02-25 18:21:42 -0800

    laying out the inspector panel with dual properties

--- editor/lib/widgets/inspector/properties/property_dual.dart
+++ editor/lib/widgets/inspector/properties/property_dual.dart
@@ -1,48 +1,121 @@
 import 'package:flutter/material.dart';
 import 'package:core/core.dart';
+import 'package:rive_editor/widgets/inherited_widgets.dart';
 import 'package:rive_editor/widgets/inspector/properties/inspector_text_field.dart';
+import 'package:rive_editor/widgets/tinted_icon.dart';
 
 class PropertyDual extends StatelessWidget {
   final List<Core> objects;
   final int propertyKeyA;
   final int propertyKeyB;
+  final String iconName;
+  final String labelA;
+  final String labelB;
   final String name;
+  final bool linkable;
 
   const PropertyDual({
     @required this.objects,
     @required this.propertyKeyA,
     @required this.propertyKeyB,
     this.name,
+    this.iconName,
+    this.linkable,
+    this.labelA = '',
+    this.labelB = '',
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(left: 20.0),
+      margin: const EdgeInsets.only(top: 5, bottom: 5),
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      height: 35,
       child: Row(
         mainAxisSize: MainAxisSize.max,
         children: [
-          Text(
-            name,
-            style: const TextStyle(color: Color(0xFF8C8C8C)),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
           Expanded(
-            child: InspectorTextField(
-              objects: objects,
-              propertyKey: propertyKeyA,
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      child: Text(
+                        name,
+                        style: RiveTheme.of(context)
+                            .textStyles
+                            .inspectorPropertyLabel,
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: linkable
+                        ? TintedIcon(
+                            icon: 'link',
+                            color: RiveTheme.of(context)
+                                .textStyles
+                                .inspectorPropertySubLabel
+                                .color,
+                          )
+                        : Container(),
+                  )
+                ],
+              ),
             ),
           ),
-          const SizedBox(
-            width: 10,
-          ),
+          const SizedBox(width: 20),
           Expanded(
-            child: InspectorTextField(
-              objects: objects,
-              propertyKey: propertyKeyB,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Flexible(
+                  flex: 1,
+                  child: InspectorTextField(
+                    objects: objects,
+                    propertyKey: propertyKeyA,
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Text(
+                    labelA,
+                    style: RiveTheme.of(context)
+                        .textStyles
+                        .inspectorPropertySubLabel,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 20),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Flexible(
+                  flex: 1,
+                  child: InspectorTextField(
+                    objects: objects,
+                    propertyKey: propertyKeyB,
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Text(
+                    labelB,
+                    style: RiveTheme.of(context)
+                        .textStyles
+                        .inspectorPropertySubLabel,
+                  ),
+                ),
+              ],
             ),
           ),
         ],
