4a9e1c22d07d98d338a878f8947342762c7f6611
Rody Davis <rody@rive.app>
2020-03-03 17:11:39 -0800

    basic api file and coop tests

--- editor/test/api_test.dart
+++ editor/test/api_test.dart
@@ -6,6 +6,10 @@ import 'dart:convert';
 import 'package:mockito/mockito.dart';
 import 'package:rive_api/api.dart';
 import 'package:rive_api/auth.dart';
+import 'package:rive_api/files.dart';
+import 'package:rive_editor/rive/file_browser/file_browser.dart';
+import 'package:rive_editor/rive/file_browser/rive_file.dart';
+import 'package:rive_editor/rive/file_browser/rive_folder.dart';
 import 'package:test/test.dart';
 import 'package:http/http.dart' as http;
 
@@ -14,6 +18,25 @@ import 'package:http/http.dart' as http;
 class RiveTestApi extends Mock implements RiveApi {
   @override
   String get host => 'https://arkham.rive.app';
+}
+
+class EditorRiveFilesTestApi extends Mock
+    implements RiveFilesApi<RiveFolder, RiveFile> {
+  FileBrowser get browser => _fileBrowser;
+  final _fileBrowser = FileBrowser();
+
+  @override
+  RiveApi get api => RiveTestApi();
+
+  @override
+  RiveFile makeFile(int id) {
+    return RiveFile(id, browser);
+  }
+
+  @override
+  RiveFolder makeFolder(Map<String, dynamic> data) {
+    return RiveFolder(data);
+  }
 }
 
 void main() {
@@ -51,6 +74,30 @@ void main() {
           ))).thenAnswer((_) async => http.Response('{"result": ""}', 200));
 
       expect(await auth.login('rive_user', '123456789'), true);
+    });
+  });
+
+  group('Check RiveFilesApi', () {
+    test('Auth login returns null', () async {
+      final files = EditorRiveFilesTestApi();
+
+      when(files.api.post(files.api.host + '/api/my/files/rive/create/'))
+          .thenAnswer((_) async => http.Response('{"result": ""}', 201));
+
+      expect(
+          files.api.post(files.api.host + '/api/my/files/rive/create/'), null);
+    });
+    test('Auth establishCoop returns null', () async {
+      final files = EditorRiveFilesTestApi();
+      final riveFile = files.makeFile(0);
+      when(files.api.post(files.api.host +
+              '/api/files/${riveFile.ownerId}/${riveFile.id}/coop'))
+          .thenAnswer((_) async => http.Response('{"result": ""}', 201));
+
+      expect(
+          files.api.post(files.api.host +
+              '/api/files/${riveFile.ownerId}/${riveFile.id}/coop'),
+          null);
     });
   });
 }
