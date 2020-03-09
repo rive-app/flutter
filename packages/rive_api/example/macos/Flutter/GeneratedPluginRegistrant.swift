b933c72005bffd28df763f03e79822c7214be1c2
Matt Sullivan <matt@rive.app>
2020-03-09 10:47:20 -0700

    Cleanup after running pub get on all packages.

--- packages/rive_api/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/rive_api/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import path_provider_macos
 import window_utils
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   WindowUtils.register(with: registry.registrar(forPlugin: "WindowUtils"))
 }
