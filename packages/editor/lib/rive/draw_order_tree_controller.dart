c15d61c2f224e6354876fd8163f8148bc1551dd8
Matt Sullivan <matt@rive.app>
2020-03-11 11:15:33 -0700

    Stripped out dragging code as it's only relevant to hierarchy

--- packages/editor/lib/rive/draw_order_tree_controller.dart
+++ packages/editor/lib/rive/draw_order_tree_controller.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
 
 import 'package:rive_core/artboard.dart';
 import 'package:rive_core/component.dart';
-import 'package:rive_core/container_component.dart';
 
 import 'package:tree_widget/flat_tree_item.dart';
 import 'package:tree_widget/tree_controller.dart';
@@ -28,95 +26,10 @@ class DrawOrderTreeController extends TreeController<Component> {
       : null;
 
   @override
-  bool allowDrop(FlatTreeItem<Component> target, DropState state,
-      List<FlatTreeItem<Component>> items) {
-    if (!super.allowDrop(target, state, items)) {
-      return false;
-    }
-
-    var desiredParent =
-        state == DropState.into ? target.data : target.data.parent;
-    for (final item in items) {
-      if (!item.data.isValidParent(desiredParent)) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  @override
-  void drop(FlatTreeItem<Component> target, DropState state,
-      List<FlatTreeItem<Component>> items) {
-    switch (state) {
-      case DropState.above:
-      case DropState.below:
-        // Set<TreeItemChildren> toSort = {};
-        var newParent = target.data.parent;
-        // First remove from existing so that proximity is preserved
-        for (final item in items) {
-          var treeItem = item.data;
-          treeItem.parent = null;
-          // treeItem.parent.children.remove(treeItem);
-        }
-        for (final item in items) {
-          var treeItem = item.data;
-          if (state == DropState.above) {
-            newParent.children.move(treeItem,
-                before: target.prev?.parent == target.parent
-                    ? target.prev.data
-                    : null,
-                after: target.data);
-          } else {
-            newParent.children.move(treeItem,
-                before: target.data,
-                after: target.next?.parent == target.parent
-                    ? target.next.data
-                    : null);
-          }
-
-          // re-parent last after changing index
-          treeItem.parent = newParent;
-          // toSort.add(newParent.children);
-        }
-
-        // for (final children in toSort) {
-        //   children.sortFractional();
-        // }
-        break;
-      case DropState.into:
-        var targetComponent = target.data;
-        if (targetComponent is! ContainerComponent) {
-          return;
-        }
-        var targetContainer = targetComponent as ContainerComponent;
-        for (final item in items) {
-          var treeItem = item.data;
-          // treeItem.parent.children.remove(treeItem);
-          // treeItem.parent = target.data;
-          targetContainer.children.moveToEnd(treeItem);
-          treeItem.parent = targetContainer;
-          // targetContainer.addChild(treeItem);
-          // target.data.children.append(treeItem);
-          // target.data.children.sortFractional();
-        }
-        break;
-      default:
-        break;
-    }
-    rive.file.value.captureJournalEntry();
-  }
-
-  @override
-  bool isDisabled(Component treeItem) => false;
+  bool isDisabled(Component treeItem) => true;
 
   @override
   bool isProperty(Component treeItem) => false;
-
-  @override
-  List<FlatTreeItem<Component>> onDragStart(
-      DragStartDetails details, FlatTreeItem<Component> item) {
-    return [item];
-  }
 
   @override
   void onMouseEnter(PointerEnterEvent event, FlatTreeItem<Component> item) =>
@@ -135,4 +48,16 @@ class DrawOrderTreeController extends TreeController<Component> {
 
   @override
   int spacingOf(Component treeItem) => 1;
+
+  @override
+  void drop(FlatTreeItem<Component> target, DropState state,
+      List<FlatTreeItem<Component>> items) {
+    // TODO: implement drop
+  }
+
+  @override
+  List<FlatTreeItem<Component>> onDragStart(
+      DragStartDetails details, FlatTreeItem<Component> item) {
+    return [];
+  }
 }
