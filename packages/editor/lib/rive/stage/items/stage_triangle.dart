35bd8352f7c2d26a00d6263fb7743b7ad72c744c
Matt Sullivan <matt@rive.app>
2020-03-13 18:41:23 -0700

    Mirroring shapes abstraction for stage shapes

--- packages/editor/lib/rive/stage/items/stage_triangle.dart
+++ packages/editor/lib/rive/stage/items/stage_triangle.dart
@@ -1,23 +1,3 @@
-import 'package:rive_core/bounds_delegate.dart';
-import 'package:rive_core/math/aabb.dart';
-import 'package:rive_core/shapes/triangle.dart';
+import 'package:rive_editor/rive/stage/items/stage_parametric_path.dart';
 
-import '../stage_item.dart';
-
-class StageTriangle extends StageItem<Triangle> with BoundsDelegate {
-  @override
-  AABB get aabb =>
-      component.shape.bounds.translate(component.artboard.originWorld);
-
-  @override
-  void boundsChanged() {
-    assert(stage != null);
-    stage.updateBounds(this);
-  }
-
-  /// A StagePath cannot be directly clicked on in the stage. It can be selected
-  /// via the hierarchy. This is because clicking over a path effectively
-  /// selects the shape (in the stage).
-  @override
-  bool get isSelectable => false;
-}
+class StageTriangle extends StageParametricPath {}
