3116e3a159fa7f753c0a697ad33aaf6204bd9215
Luigi Rosso <luigi.rosso@gmail.com>
2020-03-16 17:19:44 -0700

    Fixed formatting.

--- packages/editor/lib/widgets/inspector/properties/inspector_text_field.dart
+++ packages/editor/lib/widgets/inspector/properties/inspector_text_field.dart
@@ -100,7 +100,8 @@ class InspectorTextField<T> extends StatefulWidget {
 class _InspectorTextFieldState<T> extends State<InspectorTextField<T>> {
   final _ConvertingTextEditingController<T> _controller =
       _ConvertingTextEditingController<T>();
-  final FocusNode _focusNode = FocusNode(canRequestFocus: true, skipTraversal: true);
+  final FocusNode _focusNode =
+      FocusNode(canRequestFocus: true, skipTraversal: true);
   bool _hasFocus = false;
 
   @override
