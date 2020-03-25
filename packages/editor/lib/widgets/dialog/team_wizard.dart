3f3164f344afa2a2a424a1deb04771d3b2436bc5
Maxwell Talbot <talbot.maxwell@gmail.com>
2020-03-25 15:31:12 +0000

    made the wizard have two panels.

--- packages/editor/lib/widgets/dialog/team_wizard.dart
+++ packages/editor/lib/widgets/dialog/team_wizard.dart
@@ -17,7 +17,7 @@ Future<T> showTeamWizard<T>({BuildContext context}) {
   return showRiveDialog(
       context: context,
       builder: (context) {
-        return WizardPanel();
+        return Wizard();
       });
 }
 
@@ -30,6 +30,9 @@ enum BillingFrequency { yearly, monthly }
 
 /// The subscription team option
 enum TeamsOption { basic, premium }
+
+/// The active wizard panel
+enum WizardPanel { one, two }
 
 /// Data class for tracking data in the team subscription widget
 class TeamSubscriptionPackage {
@@ -48,8 +51,14 @@ class TeamSubscriptionPackage {
   final int minTeamNameLength = 4;
   final RegExp legalMatch = RegExp(r'^[A-Za-z0-9]+$');
 
+  bool ignoreNullName = true;
+
   /// Validates the team name
   bool validateName() {
+    if (ignoreNullName && name == null) {
+      return true;
+    }
+
     if (name == null || name == '') {
       nameValidationError = 'Please enter a valid team name.';
       return false;
@@ -68,6 +77,10 @@ class TeamSubscriptionPackage {
     return true;
   }
 
+  void attemptProgress() {
+    ignoreNullName = false;
+  }
+
   /// Validatwe the team options
   bool get validateOption => option != null;
 
@@ -76,33 +89,50 @@ class TeamSubscriptionPackage {
 }
 
 /// The main panel for holding the team wizard views
-class WizardPanel extends StatefulWidget {
+class Wizard extends StatefulWidget {
   @override
-  _WizardPanelState createState() => _WizardPanelState();
+  _WizardState createState() => _WizardState();
 }
 
-class _WizardPanelState extends State<WizardPanel> {
+class _WizardState extends State<Wizard> {
   final _sub = TeamSubscriptionPackage();
-
-  var validTeamName = true;
+  WizardPanel activePanel = WizardPanel.one;
+  void setPanel(WizardPanel panel) {
+    setState(() {
+      activePanel = panel;
+    });
+  }
 
   @override
-  void setState(fn) {
-    super.setState(fn);
-
-    validTeamName = _sub.validateName();
+  Widget build(BuildContext context) {
+    return SizedBox(
+        child: (activePanel == WizardPanel.one)
+            ? TeamWizardPanelOne(
+                sub: _sub, setState: setState, setPanel: setPanel)
+            : TeamWizardPanelTwo(
+                sub: _sub, setState: setState, setPanel: setPanel),
+        width: 452,
+        height: 376);
   }
+}
+
+class TeamWizardPanelOne extends StatelessWidget {
+  final TeamSubscriptionPackage sub;
+  final void Function(WizardPanel) setPanel;
+  final void Function(void Function()) setState;
 
   void selectOption(TeamsOption option) {
     setState(() {
-      _sub.option = option;
-      if (_sub.validateStep1) {
-        print('Safe to move onto step 2');
-      } else {
-        print('Oops! Point out what the user needs to fix!');
+      sub.option = option;
+      sub.attemptProgress();
+      if (sub.validateStep1) {
+        setPanel(WizardPanel.two);
       }
     });
   }
+
+  const TeamWizardPanelOne({Key key, this.sub, this.setState, this.setPanel})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -112,107 +142,102 @@ class _WizardPanelState extends State<WizardPanel> {
       BillingFrequency.yearly,
       BillingFrequency.monthly,
     ];
-
-    return SizedBox(
-      child: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Column(
-          children: <Widget>[
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Expanded(
-                  child: TextFormField(
-                    textAlign: TextAlign.left,
-                    textAlignVertical: TextAlignVertical.center,
-                    style: textStyles.fileGreyTextLarge,
-                    decoration: InputDecoration(
-                      isDense: true,
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                              color: colors.inputUnderline, width: 2)),
-                      hintText: 'Team name',
-                      errorText:
-                          validTeamName ? null : _sub.nameValidationError,
-                      hintStyle: textStyles.textFieldInputHint,
-                      errorStyle: textStyles.textFieldInputValidationError,
-                      contentPadding: const EdgeInsets.only(bottom: 3),
-                      filled: true,
-                      hoverColor: Colors.transparent,
-                      fillColor: Colors.transparent,
-                    ),
-                    onChanged: (name) => setState(() => _sub.name = name),
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Column(
+        children: <Widget>[
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: TextFormField(
+                  textAlign: TextAlign.left,
+                  textAlignVertical: TextAlignVertical.center,
+                  style: textStyles.fileGreyTextLarge,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: colors.inputUnderline, width: 2)),
+                    hintText: 'Team name',
+                    errorText:
+                        sub.validateName() ? null : sub.nameValidationError,
+                    hintStyle: textStyles.textFieldInputHint,
+                    errorStyle: textStyles.textFieldInputValidationError,
+                    contentPadding: const EdgeInsets.only(bottom: 3),
+                    filled: true,
+                    hoverColor: Colors.transparent,
+                    fillColor: Colors.transparent,
                   ),
+                  onChanged: (name) => setState(() => sub.name = name),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 32),
-                  child: SizedBox(
-                    width: 71,
-                    child: ComboBox<BillingFrequency>(
-                      popupWidth: 100,
-                      sizing: ComboSizing.content,
-                      underline: true,
-                      underlineColor: colors.inputUnderline,
-                      valueColor: textStyles.fileGreyTextLarge.color,
-                      options: options,
-                      value: _sub.billingFrequency,
-                      toLabel: (option) => capsFirst(describeEnum(option)),
-                      contentPadding: const EdgeInsets.only(bottom: 3),
-                      change: (option) =>
-                          setState(() => _sub.billingFrequency = option),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: validTeamName
-                  ? const EdgeInsets.only(top: 31, bottom: 31)
-                  : const EdgeInsets.only(top: 10, bottom: 31),
-              child: Row(
-                children: <Widget>[
-                  TeamSubscriptionChoiceWidget(
-                      label: 'Team',
-                      costLabel: '\$14',
-                      explanation:
-                          'A space where you and your team can share files.',
-                      onTap: () => selectOption(TeamsOption.basic)),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30),
-                    child: TeamSubscriptionChoiceWidget(
-                      label: 'Premium Team',
-                      costLabel: '\$45',
-                      explanation: '1 day support.',
-                      onTap: () => selectOption(TeamsOption.premium),
-                    ),
-                  ),
-                ],
               ),
-            ),
-            RichText(
-                text: TextSpan(
-              children: [
-                const TextSpan(
-                    text: 'You\'ll only be billed for users as'
-                        ' you add them. Read more about our '),
-                TextSpan(
-                    text: 'fair billing policy',
-                    style: textStyles.tooltipHyperlink,
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () async {
-                        if (await canLaunch(billingPolicyUrl)) {
-                          await launch(billingPolicyUrl);
-                        }
-                      }),
-                const TextSpan(text: '.'),
+              Padding(
+                padding: const EdgeInsets.only(left: 32),
+                child: SizedBox(
+                  width: 71,
+                  child: ComboBox<BillingFrequency>(
+                    popupWidth: 100,
+                    sizing: ComboSizing.content,
+                    underline: true,
+                    underlineColor: colors.inputUnderline,
+                    valueColor: textStyles.fileGreyTextLarge.color,
+                    options: options,
+                    value: sub.billingFrequency,
+                    toLabel: (option) => capsFirst(describeEnum(option)),
+                    contentPadding: const EdgeInsets.only(bottom: 3),
+                    change: (option) =>
+                        setState(() => sub.billingFrequency = option),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: sub.validateName()
+                ? const EdgeInsets.only(top: 31, bottom: 31)
+                : const EdgeInsets.only(top: 10, bottom: 31),
+            child: Row(
+              children: <Widget>[
+                TeamSubscriptionChoiceWidget(
+                    label: 'Team',
+                    costLabel: '\$14',
+                    explanation:
+                        'A space where you and your team can share files.',
+                    onTap: () => selectOption(TeamsOption.basic)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30),
+                  child: TeamSubscriptionChoiceWidget(
+                    label: 'Premium Team',
+                    costLabel: '\$45',
+                    explanation: '1 day support.',
+                    onTap: () => selectOption(TeamsOption.premium),
+                  ),
+                ),
               ],
-              style: textStyles.tooltipDisclaimer,
-            ))
-          ],
-        ),
+            ),
+          ),
+          RichText(
+              text: TextSpan(
+            children: [
+              const TextSpan(
+                  text: 'You\'ll only be billed for users as'
+                      ' you add them. Read more about our '),
+              TextSpan(
+                  text: 'fair billing policy',
+                  style: textStyles.tooltipHyperlink,
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () async {
+                      if (await canLaunch(billingPolicyUrl)) {
+                        await launch(billingPolicyUrl);
+                      }
+                    }),
+              const TextSpan(text: '.'),
+            ],
+            style: textStyles.tooltipDisclaimer,
+          ))
+        ],
       ),
-      width: 452,
-      height: 376,
     );
   }
 }
@@ -353,5 +378,29 @@ class _TeamSubscriptionChoiceWidgetState
         ),
       ),
     );
+  }
+}
+
+class TeamWizardPanelTwo extends StatelessWidget {
+  final TeamSubscriptionPackage sub;
+  final void Function(WizardPanel) setPanel;
+  final void Function(void Function()) setState;
+
+  void selectOption(TeamsOption option) {
+    setState(() {
+      sub.option = option;
+      sub.attemptProgress();
+      if (sub.validateStep1) {
+        setPanel(WizardPanel.two);
+      }
+    });
+  }
+
+  const TeamWizardPanelTwo({Key key, this.sub, this.setState, this.setPanel})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(padding: const EdgeInsets.all(30), child: Text('two'));
   }
 }
