832e73d095bab08dc647e60e293594b019197eca
Umberto Sonnino <usonnino@gmail.com>
2020-04-07 16:51:03 +0100

    Use separate object for profile management, add missing fields #177

--- packages/rive_api/lib/profiles.dart
+++ packages/rive_api/lib/profiles.dart
@@ -12,53 +12,37 @@ class RiveProfilesApi<T extends RiveOwner> {
   final log = Logger('Rive API');
   RiveProfilesApi(this.api);
 
-  Future<void> updateInfo(
-    RiveOwner owner, {
-    String name,
-    String username,
-    String location,
-    String website,
-    String bio,
-    String twitter,
-    String instagram,
-    bool isForHire,
-  }) {
+  Future<void> updateInfo(RiveOwner owner, {RiveProfile profile}) {
     if (owner is RiveTeam) {
-      return _updateTeamInfo(owner, name, username, location, website, bio,
-          twitter, instagram, isForHire);
+      return _updateTeamInfo(owner, profile);
     } else {
-      return _updateUserInfo(owner, name, username, location, website, bio,
-          twitter, instagram, isForHire);
+      return _updateUserInfo(owner, profile);
     }
   }
 
   // PUT /api/teams/<teamId>
-  Future<void> _updateTeamInfo(
-    RiveTeam team,
-    String name,
-    String username,
-    String location,
-    String website,
-    String bio,
-    String twitter,
-    String instagram,
-    bool isForHire,
-  ) async {
+  Future<void> _updateTeamInfo(RiveTeam team, RiveProfile profile) async {
     var teamId = team.ownerId;
     String payload = jsonEncode({
-      'data': {
-        'teamName': name,
-        'teamUsername': username,
-        'location': location,
-        'website': website,
-        'blurb': bio,
-        'twitter': twitter,
-        'instagram': instagram,
-      }
+      'teamName': team.name,
+      'teamUsername': team.username,
+      'location': profile.location,
+      'website': profile.website,
+      'blurb': profile.bio,
+      'twitter': profile.twitter,
+      'instagram': profile.instagram,
+      'dribbble': profile.dribbble,
+      'linkedin': profile.linkedin,
+      'behance': profile.behance,
+      'vimeo': profile.vimeo,
+      'github': profile.github,
+      'medium': profile.medium,
     });
 
+    print("Team: ${team.ownerId} $payload");
     var response =
         await api.put(api.host + '/api/teams/$teamId', body: payload);
+    print("OK? ${response.statusCode}");
     if (response.statusCode != 200) {
       // TODO: some form of error handling? also whats wrong with our error logging :D
       var message = 'Could not create new team ${response.body}';
@@ -68,17 +52,7 @@ class RiveProfilesApi<T extends RiveOwner> {
     }
   }
 
-  Future<void> _updateUserInfo(
-    RiveTeam team,
-    String name,
-    String username,
-    String location,
-    String website,
-    String bio,
-    String twitter,
-    String instagram,
-    bool isForHire,
-  ) async {
+  Future<void> _updateUserInfo(RiveTeam team, RiveProfile profile) async {
     // TODO:
     // We could use the current `/register` route,
     // but it needs some fundamental information like email that we currently
@@ -109,6 +83,6 @@ class RiveProfilesApi<T extends RiveOwner> {
     } on FormatException catch (e) {
       log.severe('Unable to parse response from server: $e');
     }
-    return RiveProfile.fromData(data);
+    return RiveProfile.fromData(data, owner);
   }
 }
