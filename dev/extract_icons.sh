ec3add988cc6ebf99a473cce30cfb4418b479e7a
Luigi Rosso <luigi.rosso@gmail.com>
2020-04-12 15:23:35 -0700

    Add png crushing to the icon extracter (reduces png sizes ~30% without quality loss).

--- dev/extract_icons.sh
+++ dev/extract_icons.sh
@@ -13,6 +13,14 @@ if [ -f "./Rive App.zip" ]; then
     rm "assets/images/icons/Rive App.png"
     rm "assets/images/icons/2.0x/Rive App.png"
     rm "assets/images/icons/3.0x/Rive App.png"
+
+    # Crush em
+    for png in `find assets/images/icons -name "*.png"`;
+    do
+        echo "crushing $png"	
+        pngcrush -reduce -brute "$png" temp.png
+        mv -f temp.png $png
+    done;
     echo 'Success!'
 else 
     echo "Failed to download 'Rive App.zip'"
