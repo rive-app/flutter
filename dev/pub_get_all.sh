4c41a586810ad14e5c88afa951e281f8d63f78c0
Maxwell Talbot <talbot.maxwell@gmail.com>
2020-04-21 09:39:38 +0100

    updated pub get all to be better

--- dev/pub_get_all.sh
+++ dev/pub_get_all.sh
@@ -1,49 +1,20 @@
 #!/usr/bin/env bash
 
-echo UPDATING utilities
-cd $RIVE_ROOT/packages/utilities && pub get test && cd -
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-echo UPDATING coop server
-cd $RIVE_ROOT/packages/coop_server && pub get test && cd -
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-echo UPDATING core
-cd $RIVE_ROOT/packages/core && pub get test && cd -
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-echo UPDATING editor
-cd $RIVE_ROOT/packages/editor && flutter pub get test && cd -
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-echo UPDATING fractional
-cd $RIVE_ROOT/packages/fractional && pub get test && cd -
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-echo UPDATING Rive API
-cd $RIVE_ROOT/packages/rive_api && flutter pub get test && cd -
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-echo UPDATING Rive core
-cd $RIVE_ROOT/packages/rive_core && flutter pub get test && cd -
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-echo UPDATING tree widget
-cd $RIVE_ROOT/packages/tree_widget && flutter pub get test && cd -
-if [ $? -ne 0 ]; then
-  exit 1
-fi
+for d in $RIVE_ROOT/packages/*/ ; do
+  folder=`basename $d`
+  echo "UPDATING $d"
+  echo "$RIVE_ROOT/packages/$folder/pubspec.yaml"
+  if grep -Fq "sdk: flutter" $RIVE_ROOT/packages/$folder/pubspec.yaml
+  then 
+    cd $RIVE_ROOT/packages/$folder && flutter pub get test && cd -
+    if [ $? -ne 0 ]; then
+      exit 1
+    fi
+  else 
+    cd $RIVE_ROOT/packages/$folder && pub get test && cd -
+    if [ $? -ne 0 ]; then
+      exit 1
+    fi
+  fi
+  
+done
