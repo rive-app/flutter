a34aecaf2255cc89a13dcc5fdf275a94078ad640
Matt Sullivan <matt@rive.app>
2020-04-25 13:47:41 -0700

    Remove borked code

--- packages/editor/lib/widgets/notifications.dart
+++ packages/editor/lib/widgets/notifications.dart
@@ -35,14 +35,6 @@ class _NotificationsPanelState extends State<NotificationsPanel> {
         padding: const EdgeInsets.only(left: 20),
         child: Column(
           children: <Widget>[
-            StreamBuilder<Uint8List>(
-              stream: ImageManagerProvider.of(context).firstImageStream,
-              builder: (context, snapshot) => snapshot.hasData
-                  ? Image.memory(snapshot.data)
-                  : const Center(
-                      child: CircularProgressIndicator(),
-                    ),
-            ),
             Expanded(
               child: Center(
                 child: SizedBox(
