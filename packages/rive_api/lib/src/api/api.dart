603263ac9fd28bb873530bb572f4bb4b63947fcf
Matt Sullivan <matt@rive.app>
2020-04-27 15:58:34 -0700

    Adds more generalized status code error handling

--- packages/rive_api/lib/src/api/api.dart
+++ packages/rive_api/lib/src/api/api.dart
@@ -7,27 +7,18 @@ class RiveApi extends WebServiceClient {
   // final String host = 'http://localhost:3000';
   final String host = 'https://stryker.rive.app';
   RiveApi() : super('rive-api') {
-    // TODO: might be nice to have a way to add a generic 'not 20X' handler
-    this.addStatusCodeHandler(400, handle_exception);
-    this.addStatusCodeHandler(401, handle_exception);
-    this.addStatusCodeHandler(403, handle_exception);
-    this.addStatusCodeHandler(500, handle_exception);
+    this.addAcceptableStatusCodes([StatusCodeClass.success], handleException);
   }
 }
 
-void handle_exception(Response response) {
-  throw ApiException(response);
-}
+void handleException(Response response) => throw ApiException(response);
 
 // Taking inspiration here
 // https://medium.com/flutter-community/handling-network-calls-like-a-pro-in-flutter-31bd30c86be1
 class ApiException implements Exception {
-  final Response _response;
-  APIError error;
-
-  ApiException(this._response) {
+  ApiException(Response response) : _response = response {
     try {
-      Map<String, dynamic> data = json.decode(_response.body);
+      final data = json.decode(_response.body) as Map<String, dynamic>;
       error = APIError(
         message: data['error']['message'],
         code: data['error']['code'],
@@ -38,18 +29,17 @@ class ApiException implements Exception {
     }
   }
 
-  String toString() {
-    return '[${_response.statusCode}] ${_response.request.url}: ${_response.body.toString()}';
-  }
+  final Response _response;
+  APIError error;
 
-  String displayError() {
-    return _response.body.toString();
-  }
+  String toString() =>
+      '[${_response.statusCode}] ${_response.request.url}: ${_response.body.toString()}';
+
+  String displayError() => _response.body.toString();
 }
 
 class APIError {
+  APIError({this.message, this.code});
   final String message;
   final String code;
-
-  APIError({this.message, this.code});
 }
