402e92ac2d0e24a61b6f8db87d3dfe50cf309c30
Luigi Rosso <luigi.rosso@gmail.com>
2020-06-01 13:06:56 -0700

    Cleanup script.

--- dev/pack_icons.sh
+++ dev/pack_icons.sh
@@ -4,5 +4,3 @@ if [[ ! -f "./pack_icons_bin" || "$1" == "build" ]]; then
 fi
 rm -fr ../packages/editor/assets/images/icon_atlases
 ./pack_icons_bin ../packages/editor/assets/images/icons  ../packages/editor/assets/images/icon_atlases ../packages/editor/lib/packed_icon.dart
-#./pack_icons_bin ../packages/editor/assets/images/icons/2.0x  ../packages/editor/assets/images/icon_atlases/2x
-#./pack_icons_bin ../packages/editor/assets/images/icons/3.0x  ../packages/editor/assets/images/icon_atlases/3x
