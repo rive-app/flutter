43e9537dcf06b93a8261a1cd3f54b6fcf185e7c5
Luigi Rosso <luigi.rosso@gmail.com>
2020-06-19 13:57:21 -0700

    Test for #789.

--- packages/editor/test/helpers/test_open_file_context.dart
+++ packages/editor/test/helpers/test_open_file_context.dart
@@ -1,10 +1,11 @@
 import 'package:rive_core/rive_file.dart';
 import 'package:local_data/local_data.dart';
 import 'package:rive_editor/rive/open_file_context.dart';
+import 'package:rive_editor/rive/rive.dart';
 
 /// A fake open file context that's valid and usable for testing.
 class TestOpenFileContext extends OpenFileContext {
-  TestOpenFileContext() : super(10, 10);
+  TestOpenFileContext() : super(10, 10, rive: Rive());
   Future<bool> fakeConnect() async {
     LocalDataPlatform dataPlatform = LocalDataPlatform.make();
     core = TestRiveFile('fake', localDataPlatform: dataPlatform);
---
+++ packages/editor/test/tool_test.dart
@@ -0,0 +1,94 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rive_core/math/mat2d.dart';
+import 'package:rive_core/math/vec2d.dart';
+import 'package:rive_core/artboard.dart';
+import 'package:rive_core/shapes/ellipse.dart';
+import 'package:rive_core/shapes/shape.dart';
+import 'package:rive_editor/rive/open_file_context.dart';
+import 'package:rive_editor/rive/stage/tools/auto_tool.dart';
+import 'package:rive_editor/rive/stage/tools/ellipse_tool.dart';
+import 'package:rive_editor/rive/stage/stage_item.dart';
+
+import 'helpers/test_open_file_context.dart';
+
+Future<OpenFileContext> _makeFile() async {
+  var file = TestOpenFileContext();
+  expect(await file.fakeConnect(), true);
+  Artboard artboard;
+  var core = file.core;
+  core.batchAdd(() {
+    artboard = Artboard()
+      ..name = 'My Artboard'
+      ..x = 0
+      ..y = 0
+      ..width = 1920
+      ..height = 1080;
+
+    core.addObject(artboard);
+  });
+  core.captureJournalEntry();
+  return file;
+}
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  test('create ellipse test', () async {
+    var file = await _makeFile();
+    var core = file.core;
+    var stage = file.stage;
+
+    // Place the mouse at an initial position...
+    stage.mouseMove(1, 100, 100);
+
+    // Select the pen tool and expect it to validate.
+    stage.tool = EllipseTool.instance;
+    expect(stage.tool, EllipseTool.instance);
+
+    // Click and drag from 100, 100 to 300, 200
+    stage.mouseDown(1, 100, 100);
+    stage.mouseDrag(1, 100, 100);
+
+    stage.mouseDrag(1, 300, 200);
+    stage.mouseUp(1, 300, 200);
+
+    expect(core.objectsOfType<Shape>().length, 1,
+        reason: 'there should be one shape after clicking the pen tool');
+    expect(core.objectsOfType<Ellipse>().length, 1,
+        reason: 'there should be one ellipse');
+
+    var shape = core.objectsOfType<Shape>().first;
+    var ellipse = core.objectsOfType<Ellipse>().first;
+    expect(
+        Mat2D.areEqual(shape.worldTransform,
+            Mat2D.fromTranslation(Vec2D.fromValues(100, 100))),
+        true);
+    expect(ellipse.width, 200);
+    expect(ellipse.height, 100);
+
+    file.advance(0);
+
+    stage.tool = AutoTool.instance;
+    // Move mouse to trigger hover...
+    stage.mouseMove(1, 150, 150);
+    expect(stage.hoverItem, shape.stageItem);
+
+    // Select hovered item
+    stage.mouseDown(1, 150, 150);
+    expect(file.selection.items.contains(shape.stageItem), true,
+        reason: 'should\'ve selected the ellipse shape');
+
+    stage.mouseDrag(1, 150, 150);
+
+    // move 50 pixels on x
+    stage.mouseDrag(1, 200, 150);
+    stage.mouseUp(1, 200, 150);
+    file.advance(0);
+
+    expect(
+        Mat2D.areEqual(shape.worldTransform,
+            Mat2D.fromTranslation(Vec2D.fromValues(150, 100))),
+        true,
+        reason:
+            'expect the shape to have moved 50 pixels from 100 to 150 on x');
+  });
+}
