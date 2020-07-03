fadfa5eb6aecdeefb373c6de2db6ad7599d83a0a
Luigi Rosso <luigi.rosso@gmail.com>
2020-07-03 09:57:07 -0700

    Small cleanup to gitignore and dev tools so generated aot bin files are ignored.

--- dev/generate_core_runtime.sh
+++ dev/generate_core_runtime.sh
@@ -1,8 +1,9 @@
 
-if [[ ! -f "./core_generator_bin" || "$1" == "build" ]]; then
-    dart2native ../packages/core_generator/lib/main.dart -o core_generator_bin
+if [[ ! -f "./bin/core_generator" || "$1" == "build" ]]; then
+    mkdir -p ./bin
+    dart2native ../packages/core_generator/lib/main.dart -o ./bin/core_generator
 fi
-./core_generator_bin  --definitions-folder=./defs/ --core-context=RiveCoreContext --output-folder=../packages/rive_flutter_runtime/ --package=rive --runtime --runtime-core-folder=../packages/rive_core/
+./bin/core_generator  --definitions-folder=./defs/ --core-context=RiveCoreContext --output-folder=../packages/rive_flutter_runtime/ --package=rive --runtime --runtime-core-folder=../packages/rive_core/
 # The core generate can leave behind some dangling imports, use our tool to remove them.
 ./remove_unused_imports.sh ../packages/rive_flutter_runtime
 # Some core properties would take a bunch of extra code to annotate, so let's just let dartfix do this for us.
--- dev/pack_icons.sh
+++ dev/pack_icons.sh
@@ -1,6 +1,7 @@
 
-if [[ ! -f "./pack_icons_bin" || "$1" == "build" ]]; then
-    dart2native ../packages/pack_icons/lib/main.dart -o pack_icons_bin
+if [[ ! -f "./bin/pack_icons" || "$1" == "build" ]]; then
+    mkdir -p ./bin
+    dart2native ../packages/pack_icons/lib/main.dart -o ./bin/pack_icons
 fi
 rm -fr ../packages/editor/assets/images/icon_atlases
-./pack_icons_bin ../packages/editor/assets/images/icons  ../packages/editor/assets/images/icon_atlases ../packages/editor/lib/packed_icon.dart
+./bin/pack_icons ../packages/editor/assets/images/icons  ../packages/editor/assets/images/icon_atlases ../packages/editor/lib/packed_icon.dart
