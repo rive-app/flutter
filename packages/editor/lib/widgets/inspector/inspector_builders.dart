28ee9f5517c90ce77f16410e946b9a1bc6df556f
Matt Sullivan <matt@rive.app>
2020-07-06 14:09:24 -0700

    Hide alignment inspector

--- packages/editor/lib/widgets/inspector/inspector_builders.dart
+++ packages/editor/lib/widgets/inspector/inspector_builders.dart
@@ -1,6 +1,6 @@
 import 'package:rive_editor/widgets/inspector/backboard_inspector_builder.dart';
 import 'package:rive_editor/widgets/inspector/fills_inspector_builder.dart';
-import 'package:rive_editor/widgets/inspector/alignment_inspector_builder.dart';
+// import 'package:rive_editor/widgets/inspector/alignment_inspector_builder.dart';
 import 'package:rive_editor/widgets/inspector/artboard_inspector_builder.dart';
 import 'package:rive_editor/widgets/inspector/inspect_blend.dart';
 import 'package:rive_editor/widgets/inspector/inspect_transform.dart';
@@ -11,7 +11,7 @@ import 'package:rive_editor/widgets/inspector/strokes_inspector_builder.dart';
 /// The list of possible inspector builders, these are iterated, validated, and
 /// expanded when the inspector is built from the selection set.
 final List<InspectorBuilder> defaultInspectorBuilders = [
-  AlignmentInspectorBuilder(),
+  // AlignmentInspectorBuilder(),
   BackboardInspectorBuilder(),
   ArtboardInspectorBuilder(),
   TransformInspectorBuilder(),
