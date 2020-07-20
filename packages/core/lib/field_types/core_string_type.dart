15758856659af88c919cf8ce9f6797b3be74a321
Luigi Rosso <luigi.rosso@gmail.com>
2020-07-19 18:06:04 -0700

    Fixes #973 -> revs runtime to file version to 2

--- packages/core/lib/field_types/core_string_type.dart
+++ packages/core/lib/field_types/core_string_type.dart
@@ -6,15 +6,25 @@ import 'package:utilities/binary_buffer/binary_writer.dart';
 
 class CoreStringType extends CoreFieldType<String> {
   @override
-  String deserialize(BinaryReader reader) => reader.readString();
+  String deserialize(BinaryReader reader) {
+    // TODO: replace with this in a few days:
+    // return reader.readString(explicitLength: false);
+
+    // Temporary workaround:
+    var actualReader = BinaryReader(reader.buffer);
+    var hackyTestReader = BinaryReader(reader.buffer);
+    var oldStyle =
+        hackyTestReader.readVarUint() == reader.buffer.lengthInBytes - 1;
+    return actualReader.readString(explicitLength: oldStyle);
+  }
 
   @override
   String lerp(String from, String to, double f) => from;
 
   @override
   Uint8List serialize(String value) {
-    var writer = BinaryWriter(alignment: 32);
-    writer.writeString(value);
+    var writer = BinaryWriter(alignment: value.length * 2);
+    writer.writeString(value, explicitLength: false);
     return writer.uint8Buffer;
   }
 }
