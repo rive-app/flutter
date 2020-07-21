ff9ed11cdbf05a5a7daac7e38e2880682e6bb6fe
Matt Sullivan <matt@rive.app>
2020-07-21 08:24:44 -0700

    Docker updated to build from tagged Flutter version

--- dev/update_flutter.sh
+++ dev/update_flutter.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# exit on error
+set -e
+
 # FLUTTER_ROOT must be set
 if [[ -z ${FLUTTER_ROOT+x} ]]; then
     echo "FLUTTER_ROOT is unset"
-    return
+    exit 1
 fi
 
 # RIVE_ROOT must be set
 if [[ -z ${RIVE_ROOT+x} ]]; then
     echo "RIVE_ROOT is unset"
-    return
+    exit 1
 fi
 
 VERSION_FILE=".flutter_version"
@@ -17,14 +20,17 @@ VERSION_FILE=".flutter_version"
 # .flutter_version must exist
 if [[ ! -f "$RIVE_ROOT/$VERSION_FILE" ]]; then
     echo "$VERSION_FILE doesn't exist"
-    return
+    exit 1
 fi
 
 # read the flutter version from the file
 TAG=$(<$RIVE_ROOT/$VERSION_FILE)
 echo $TAG
 
-cd $FLUTTER_ROOT
+pushd $FLUTTER_ROOT
+
+# update the local repository with new info from github
+git fetch
 
 # set the correct tag
 git checkout tags/$TAG
