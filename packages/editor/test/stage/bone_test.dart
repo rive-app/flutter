d762c29e2b2c9ba09d27c09c87f8ca6f18677670
Matt Sullivan <matt@rive.app>
2020-08-27 11:29:39 -0700

    Bone and translate tool tests

--- packages/editor/test/stage/bone_test.dart
+++ packages/editor/test/stage/bone_test.dart
@@ -8,6 +8,7 @@ import 'package:rive_editor/rive/shortcuts/shortcut_actions.dart';
 import 'package:rive_editor/rive/stage/tools/auto_tool.dart';
 import 'package:rive_editor/rive/stage/tools/bone_tool.dart';
 import 'package:rive_editor/rive/stage/stage_item.dart';
+import 'package:rive_editor/rive/stage/tools/translate_tool.dart';
 
 import '../helpers/test_helpers.dart';
 
@@ -46,7 +47,6 @@ void main() {
         reason: 'a root bone should have been created');
 
     final rootBone = core.objectsOfType<RootBone>().first;
-    final stageRootBone = rootBone.stageItem;
 
     // root bone properties are correct?
     expect(rootBone.x, 100);
@@ -97,5 +97,78 @@ void main() {
 
     // auto tool should now be selected
     expect(stage.tool, AutoTool.instance);
+  });
+
+  test('Bones can be transformed', () async {
+    final file = await makeFile(); // also makes an artboard
+    final core = file.core;
+    final stage = file.stage;
+
+    // select the bone tool
+    stage.tool = instance;
+
+    // click at (100, 100)
+    stage.mouseMove(1, 100, 100);
+    stage.mouseDown(1, 100, 100);
+    stage.mouseUp(1, 100, 100);
+
+    // click at (200, 200)
+    stage.mouseMove(1, 200, 200);
+    stage.mouseDown(1, 200, 200);
+    stage.mouseUp(1, 200, 200);
+
+    // the tool should still be the bone tool when mouse is released
+    expect(stage.tool, BoneTool.instance);
+
+    // we should now have a root bone
+    expect(core.objectsOfType<RootBone>().length, 1,
+        reason: 'a root bone should have been created');
+
+    final rootBone = core.objectsOfType<RootBone>().first;
+    final stageRootBone = rootBone.stageItem;
+    expect(stageRootBone, isNotNull);
+
+    // click at (200, 300)
+    stage.mouseMove(1, 200, 300);
+    stage.mouseDown(1, 200, 300);
+    stage.mouseUp(1, 200, 300);
+
+    final firstBone = core.objectsOfType<Bone>().toList()[0];
+    final stageFirstBone = firstBone.stageItem;
+    expect(stageFirstBone, isNotNull);
+
+    // click at (300, 300)
+    stage.mouseMove(1, 300, 300);
+    stage.mouseDown(1, 300, 300);
+    stage.mouseUp(1, 300, 300);
+
+    final secondBone = core.objectsOfType<Bone>().toList()[0];
+    final stageSecondBone = secondBone.stageItem;
+    expect(stageSecondBone, isNotNull);
+
+    // hit escape
+    file.triggerAction(ShortcutAction.cancel);
+
+    // Root bone should be at (100, 100)
+    expect(rootBone.x, 100);
+    expect(rootBone.y, 100);
+
+    // Activate the translation tool
+    stage.tool = TranslateTool.instance;
+
+    // Select the root bone
+    file.select(stageRootBone);
+    expect(file.selection.first, stageRootBone);
+
+    // Translate by (50, 50)
+    stage.mouseMove(1, 0, 0);
+    stage.mouseDown(1, 0, 0);
+    stage.mouseDrag(1, 0, 0);
+    stage.mouseDrag(1, 50, 50);
+    stage.mouseUp(1, 50, 50);
+
+    // Root bone should now be at (150, 150)
+    expect(rootBone.x, 150);
+    expect(rootBone.y, 150);
   });
 }
