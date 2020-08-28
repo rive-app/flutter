ccf1721ffe35b906204e79ae2b1098d4ab1a57cf
Luigi Rosso <luigi.rosso@gmail.com>
2020-08-28 11:03:23 -0700

    Removing old string decoding detector as it’s causing issues.

--- packages/core/lib/field_types/core_string_type.dart
+++ packages/core/lib/field_types/core_string_type.dart
@@ -7,15 +7,7 @@ import 'package:utilities/binary_buffer/binary_writer.dart';
 class CoreStringType extends CoreFieldType<String> {
   @override
   String deserialize(BinaryReader reader) {
-    // TODO: replace with this in a few days:
-    // return reader.readString(explicitLength: false);
-
-    // Temporary workaround:
-    var actualReader = BinaryReader(reader.buffer);
-    var hackyTestReader = BinaryReader(reader.buffer);
-    var oldStyle =
-        hackyTestReader.readVarUint() == reader.buffer.lengthInBytes - 1;
-    return actualReader.readString(explicitLength: oldStyle);
+    return reader.readString(explicitLength: false);
   }
 
   @override
