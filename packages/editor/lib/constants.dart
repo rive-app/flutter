6c2bbf73fe3ab0438bf8b0dfd28d7d388908d23f
Luigi Rosso <luigi.rosso@gmail.com>
2020-10-02 15:20:19 -0700

    Improve node ux (doubleClick speed and expanding paths).

--- packages/editor/lib/constants.dart
+++ packages/editor/lib/constants.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 const kDefaultWIndowSize = Size(1366, 768);
 
 /// This is the maximum time that can elapse between two clicks to consider them
 /// a double click.
-const doubleClickSpeed = Duration(milliseconds: 200);
+const doubleClickSpeed = Duration(milliseconds: 400);
