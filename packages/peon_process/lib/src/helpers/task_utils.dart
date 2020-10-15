93ca8e0efc454417b317ee08921ab990b3a394d4
Maxwell Talbot <talbot.maxwell@gmail.com>
2020-10-15 13:14:26 +0100

    wrapping peon tasks to always try to report back success/failure to our backend

--- packages/peon_process/lib/src/helpers/task_utils.dart
+++ packages/peon_process/lib/src/helpers/task_utils.dart
@@ -8,15 +8,31 @@ abstract class PeonTask with Task {
   final Map originalTaskData;
 
   PeonTask({this.taskId, this.originalTaskData});
+
+  // actual task implementation
+  Future<bool> peonExecute();
+
+  @override
+  Future<bool> execute() async {
+    bool success = false;
+    try {
+      success = await peonExecute();
+    } finally {
+      // make sure we update our task if we're completed here...
+      await completeTask(this, success);
+    }
+    return success;
+  }
 }
 
-Future<void> completeTask(PeonTask task) async {
+Future<void> completeTask(PeonTask task, bool success) async {
   var client = ConsoleClient();
   try {
     var queue = await getJSQueue(client);
+    var work = success ? 'TaskCompleted' : 'TaskFailed';
     await queue.sendMessage(
       json.encode({
-        'work': 'TaskCompleted',
+        'work': work,
         'taskId': task.taskId,
         'payload': task.originalTaskData
       }),
