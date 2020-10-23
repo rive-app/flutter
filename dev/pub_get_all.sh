525c8c9e66bd9be9d3d027703c4ad174b70e2cde
Matt Sullivan <matt@rive.app>
2020-10-23 11:33:52 -0400

    Updating pubspecs, fixing update script

--- dev/pub_get_all.sh
+++ dev/pub_get_all.sh
@@ -3,12 +3,9 @@
 list=`find $RIVE_ROOT/packages -type f -name 'pubspec.yaml' | sed -E 's|/[^/]+$||'`
 echo "$list"
 for d in  $list; do
-  echo "$d"
-  folder=`basename $d`
   echo "UPDATING $d"
-  echo "$d/pubspec.yaml"
   if grep -Fq "sdk: flutter" $d/pubspec.yaml
-  then 
+  then
     cd $d && flutter pub get test && cd -
     if [ $? -ne 0 ]; then
       exit 1
@@ -19,5 +16,4 @@ for d in  $list; do
       exit 1
     fi
   fi
-  
 done
