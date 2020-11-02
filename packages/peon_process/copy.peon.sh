acd72f13b658f14b4888bfce54741e347df966a3
Maxwell Talbot <talbot.maxwell@gmail.com>
2020-11-02 15:12:46 +0000

    addign svgcleaner back into the mix for peon build, :facepalm:

--- packages/peon_process/copy.peon.sh
+++ packages/peon_process/copy.peon.sh
@@ -2,3 +2,4 @@
 set -e 
 cp -r /app/packages/peon_process/build/linux/release/ /output/release
 cp /usr/local/bin/rive_thumbnail_generator /output/rive_thumbnail_generator
+cp /root/.cargo/bin/svgcleaner /output/svgcleaner
